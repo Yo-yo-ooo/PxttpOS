ad5f5af4b2493439f9ad4ec30c7a694dfca7b7e7
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2023-10-02 22:12:39 +0800

    6

--- libm/IO/io.h
+++
@@ -1,9 +0,0 @@
-#include "../syscallManager.h"
-
-typedef __builtin_va_list       va_list;
-#define va_start(ap, X)         __builtin_va_start(ap, X)
-#define va_arg(ap, type)        __builtin_va_arg(ap, type)
-#define va_end(ap)              __builtin_va_end(ap)
-
-int getchar();
-int printf(const char* str, ...);
--- modules/desktop/main.cpp
+++ modules/desktop/main.cpp
@@ -8,26 +8,61 @@
 #include <libm/syscallManager.h>
 #include <libm/rendering/basicRenderer.h>
 #include <libm/rendering/Cols.h>
-#include <libm/IO/io.h>
 #include <libm/msgPackets/keyPacket/keyPacket.h>
 #include "keyscan.h"
 
-TempRenderer *render;
 
-int getInput(){
-    //char *input;
-    GenericMessagePacket *msg = msgGetMessage();
-    if(msg->Type == MessagePacketType::KEY_EVENT){
-        KeyMessagePacket *keyMsg = (KeyMessagePacket*)msg->Data;
-        if(keyMsg->Type == KeyMessagePacketType::KEY_RELEASE){
-            //HandleKeyboard(keyMsg->Scancode);
-            int key = keyMsg->Scancode;
-            msg->Free();
-            _Free(msg);
-            return key;
+
+
+void PutASCII(TempRenderer *renderer){
+    int msgCount = min(msgGetCount(), 10);
+    for (int i = 0; i < msgCount; i++)
+    {
+        GenericMessagePacket *msg = msgGetMessage();
+        if (msg == NULL)
+            break;
+        if(msg->Type == MessagePacketType::KEY_EVENT){
+            KeyMessagePacket *keyMsg = (KeyMessagePacket*)msg->Data;
+            bool IsLeftShiftPressed;
+            bool IsRightShiftPressed;
+            if(keyMsg->Type == KeyMessagePacketType::KEY_RELEASE){
+                switch (keyMsg->Scancode){
+                    case LeftShift:
+                        IsLeftShiftPressed = true;
+                        return;
+                    case LeftShift + 0x80:
+                        IsLeftShiftPressed = false;
+                        return;
+                    case RightShift:
+                        IsRightShiftPressed = true;
+                        return;
+                    case RightShift + 0x80:
+                        IsRightShiftPressed = false;
+                        return;
+                    case Enter:
+                        renderer->Println();
+                        return;
+                    case Spacebar:
+                        renderer->Print(' ');
+                        return;
+                    case Backspace:
+                        renderer->ClearChar();
+                        return;
+                }
+
+                char ASCII = Translate(keyMsg->Scancode, IsLeftShiftPressed | IsRightShiftPressed);
+
+                if (ASCII != 0){
+                    renderer->Print(ASCII);
+                }
+                msg->Free();
+                _Free(msg);
+            }
         }
     }
 }
+
+TempRenderer *render;
 
 int main(){
     programSetPriority(2);
@@ -37,7 +72,6 @@ int main(){
     render->Clear(Colors.black);
     render->Println("Welcome to PxttpOS Termainal!");
     while(true){
-        //printf(">");
-        HandleKeyboard(getInput());
+        PutASCII(render);
     }
 }
