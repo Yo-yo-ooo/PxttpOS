3e8dfaaa3f249df43c187b0a0da93d3b452033ab
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2023-12-22 19:23:35 +0800

    memory

--- libm/stdlib/stdlib.h
+++ libm/stdlib/stdlib.h
@@ -1,5 +1,13 @@
 #pragma once
 
+#include <stdint.h>
+#include <stddef.h>
 
 int atoi(char* pstr);
 double atof(const char *str);
+
+//memory
+void* malloc(size_t size);
+void* calloc(size_t num, size_t size);
+void* realloc(void* ptr, size_t size);
+void free(void* ptr);
